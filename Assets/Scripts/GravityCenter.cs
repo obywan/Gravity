87a57c52b7de046d29ebe4104c46f1c768f85e4a
obywan <golodyukoleg@gmail.com>
2020-11-03 17:02:06 +0200

    Initial commit

---
+++ Assets/Scripts/GravityCenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCenter : MonoBehaviour
+{
+    public double GRVT = 6.67 * 0.00000000001f;
+
+    public float mass = 1f;
+
+    private void FixedUpdate()
+    {
+        ApplyForceToObjects();
+    }
+
+    private void ApplyForceToObjects()
+    {
+        if (Projectile.projectiles != null)
+        {
+            for(int i = 0; i < Projectile.projectiles.Count; i++)
+            {
+                Projectile.projectiles[i].AddForce(GetForce(Projectile.projectiles[i].Mass, Projectile.projectiles[i].Pos));
+            }
+        }
+            
+    }
+
+    private Vector3 GetForce(float m, Vector3 pos)
+    {
+        Vector3 direction = (transform.position - pos).normalized;
+        float r = Vector3.Distance(transform.position, pos);
+
+        return direction * ((float)GRVT * (mass * m) / r * r);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public static List<Projectile> projectiles;
+
+    private Rigidbody rb;
+    private Vector3 startPos;
+
+    public float Mass
+    {
+        get
+        {
+            return rb.mass;
+        }
+    }
+
+    public Vector3 Pos
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+
+    private void Awake()
+    {
+        if(projectiles == null)
+        {
+            projectiles = new List<Projectile>();
+        }
+    }
+
+    private void OnEnable()
+    {
+        projectiles.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        projectiles.Remove(this);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            AddForce(Vector3.left * 10f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            AddForce(Vector3.right * 10f);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            ResetPos();
+        }
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        rb.AddForce(force, ForceMode.Acceleration);
+    }
+
+    private void ResetPos()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = startPos;
+    }
+}
