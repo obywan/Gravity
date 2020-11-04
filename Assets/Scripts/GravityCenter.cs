3a698d91009651abcf3be599713104e51fd191cc
obywan <golodyukoleg@gmail.com>
2020-11-04 16:49:13 +0200

    some tweaks

--- Assets/Scripts/GravityCenter.cs
+++ Assets/Scripts/GravityCenter.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class GravityCenter : MonoBehaviour
 {
     public double GRVT = 6.67 * 0.00000000001f;
-
     public float mass = 1f;
+
+    public float distanceValue = 1f;
 
     private void FixedUpdate()
     {
@@ -20,17 +21,17 @@ public class GravityCenter : MonoBehaviour
         {
             for(int i = 0; i < Projectile.projectiles.Count; i++)
             {
-                Projectile.projectiles[i].AddForce(GetForce(Projectile.projectiles[i].Mass, Projectile.projectiles[i].Pos));
+                Projectile.projectiles[i].AddForce(GetNormalGravityForce(Projectile.projectiles[i].Mass, Projectile.projectiles[i].Pos));
             }
         }
             
     }
 
-    private Vector3 GetForce(float m, Vector3 pos)
+    private Vector3 GetNormalGravityForce(float m, Vector3 pos)
     {
         Vector3 direction = (transform.position - pos).normalized;
         float r = Vector3.Distance(transform.position, pos);
 
-        return direction * ((float)GRVT * (mass * m) / r * r);
+        return direction * ((float)GRVT * (mass * m) / r * r * distanceValue);
     }
 }
